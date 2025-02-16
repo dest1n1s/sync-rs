a616b584c6701cc7fb719a21710b5d64c3c69a58
Dest1n1 <dest1nis2022@gmail.com>
2025-02-17 02:18:10 +0800

    chore: fix git commit order

--- release.sh
+++ release.sh
@@ -14,9 +14,9 @@ cargo install cargo-edit 2>/dev/null || true
 cargo set-version $1
 
 # Tag release
-git tag v$(cargo metadata --format-version 1 | jq -r '.packages[] | select(.name=="sync-rs") | .version') -m "Release v$1"
 git add .
 git commit -m "chore: Release v$1"
+git tag v$(cargo metadata --format-version 1 | jq -r '.packages[] | select(.name=="sync-rs") | .version') -m "Release v$1"
 git push origin --tags
 
 
